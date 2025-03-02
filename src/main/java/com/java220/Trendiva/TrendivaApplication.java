d6b801afbcc28c1191a603f2b7e62382a1621718
furkanhocavektorel <furkanhocavektorel@gmail.com>
2025-03-02 10:52:06 +0300

    Initial Commit

---
+++ src/main/java/com/java220/Trendiva/TrendivaApplication.java
@@ -0,0 +1,13 @@
+package com.java220.Trendiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java220/Trendiva/TrendivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java220.Trendiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
